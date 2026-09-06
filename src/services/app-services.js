b6ca2d28c0bd7876f799e4cb1b412e3053d62ce8
KrisCea <cris.cea@duocuc.cl>
2026-09-06 02:44:27 -0300

    Se corrigen errores por problemas con tailwind

--- src/services/app-services.js
+++
@@ -1,7 +0,0 @@
-// Composicion de servicios de la aplicacion.
-// Este archivo sera el punto de reemplazo cuando se habilite el backend.
-import { createContentRepository } from '../repositories/content-repository.js';
-
-export const appServices = Object.freeze({
-  contentRepository: createContentRepository()
-});
